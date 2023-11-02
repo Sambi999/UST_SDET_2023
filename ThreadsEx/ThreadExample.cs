619e2aff7db492adaa67a06776f9c584e68b1013
Sambi999 <206217@ust.com>
2023-11-03 02:08:50 +0530

    firstcommit

---
+++ ThreadsEx/ThreadExample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadsEx
+{
+    internal class ThreadExample
+    {
+        static void Main()
+        {
+            Thread thread1 = new Thread(T1);
+            Thread thread2 = new Thread(T1);
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            Console.WriteLine("Both threads have completed their work.");
+        }
+        static void T1()
+        {
+            for (int i=1; i<=5;  i++)
+            {
+                Console.WriteLine("Thread " + Thread.CurrentThread.ManagedThreadId + "is working: " +i);
+                Thread.Sleep(100);//Simulate some work
+            }
+        }
+
+    }
+}
