5ab3ddfefc9b680f08d954a5edd90772595bf965
Sambi999 <206217@ust.com>
2023-11-04 23:10:49 +0530

    firstcommit

---
+++ CaseStudy3/Course.cs
@@ -0,0 +1,55 @@
+﻿using CaseStudy3.Exception;
+using static CaseStudy3.Exception.EnrollmentException;
+
+namespace CaseStudy3
+{
+    internal class Course : IEnrollable
+    {
+
+
+        public int CourseCode { get; set; }
+        public string? Title { get; set; }
+        public string? Instructor { get; set; }
+        public int? MaxCount { get; set; }
+        public static List<Course> courses = new List<Course>();
+        public List<Student> Enrollments = new List<Student>();
+
+        public void CourseRegistration(int id, int sid)
+        {
+            var data = courses.Find(x => x.CourseCode == id && x.MaxCount >= x.Enrollments.Count);
+            var nameCheck = Student.students.Find(x => x.Id == sid);
+            var s = data.Enrollments.Find(x => x.Id == sid);
+
+            if (data == null)
+            {
+                throw new FullException(EnrollmentException.exceptionMessages["One"]);
+            }
+            else if (s != null)
+            {
+                throw new DuplicateException(EnrollmentException.exceptionMessages["Two"]);
+            }
+            else
+            {
+                data.Enrollments.Add(nameCheck);
+                Console.WriteLine("Successfully Added!!!");
+            }
+
+        }
+
+        public void CourseWithdrawal(int id)
+        {
+            var data = courses.Find(x => x.CourseCode == id);
+            var removeEnroll = data.Enrollments.RemoveAll(x => x.Id == id);
+            if (removeEnroll != null)
+            {
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.WriteLine("Unsuccess");
+            }
+        }
+
+    }
+
+}
---
+++ CaseStudy3/Exception/EnrollmentException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseStudy3.Exception
+{
+    internal class EnrollmentException
+    {
+        public static Dictionary<string, string> exceptionMessages = new Dictionary<string, string>()
+        {
+            {"One","The Course Enrolled Is Full sorry For The Inconvenience" },
+            {"Two","Duplicate Entry Not Allowed" }
+
+        };
+        internal class FullException : IOException
+        {
+            public FullException(string message) : base(message)
+            {
+
+            }
+        }
+        internal class DuplicateException : IOException
+        {
+            public DuplicateException(string message) : base(message)
+            {
+
+            }
+        }
+
+    }
+}
